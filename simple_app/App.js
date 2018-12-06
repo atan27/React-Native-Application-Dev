ce61896ebb69beba4176887718eb27fc2e8ef1a1
atan27 <atan27@illinois.edu>
2018-12-05 19:54:54 -0600

    push me button

---
+++ simple_app/App.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { TouchableOpacity, StyleSheet, View, Text } from 'react-native'
+
+const App = () => {
+   return (
+      <View style = {styles.container}>
+         <TouchableOpacity>
+            <Text style = {styles.text}>
+               Press Me!
+            </Text>
+         </TouchableOpacity>
+      </View>
+   )
+}
+export default App
+
+const styles = StyleSheet.create ({
+   container: {
+     flex: 1,
+     flexDirection: 'column',
+     justifyContent: 'center',
+     alignItems: 'center',
+   },
+   text: {
+      borderWidth: 1,
+      padding: 25,
+      borderColor: 'black',
+      backgroundColor: 'white'
+   }
+})
+
+//const styles = StyleSheet.create({
+  //container: {
+    //flex: 1,
+    //backgroundColor: '#fff',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  //},
+//});
